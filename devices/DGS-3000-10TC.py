8a55b2cfb06089265135f38a36f40438760d42a6
mxmp <bandoftoys@gmail.com>
2019-10-29 08:25:39 +0400

    [DGS-3000-10TC] Немножко форматирования в модуле

--- devices/DGS-3000-10TC.py
+++ devices/DGS-3000-10TC.py
@@ -130,7 +130,7 @@ cdPairStatus = ({
                 },)
 
 get_HardwareRev = {
-    #    HardwareRev         .1.3.6.1.2.1.16.19.3.0				probeHardwareRev
+    # HardwareRev    .1.3.6.1.2.1.16.19.3.0				probeHardwareRev
     'HardwareRev.': '.1.3.6.1.2.1.16.19.3.0',
 }
 
@@ -140,182 +140,152 @@ walk_PortIndex = {
 }
 
 get_PortIndex = {
-    #    PortIndex           .1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1		swL2PortInfoPortIndex
-    'PortIndex..1': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.1.1',
-    'PortIndex..2': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.2.1',
-    'PortIndex..3': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.3.1',
-    'PortIndex..4': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.4.1',
-    'PortIndex..5': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.5.1',
-    'PortIndex..6': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.6.1',
-    'PortIndex..7': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.7.1',
-    'PortIndex..8': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.8.1',
-    'PortIndex..9': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.9.1',
-    'PortIndex..10': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.10.1',
-    'PortIndex..11': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.11.1',
-    'PortIndex..12': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.12.1',
-    'PortIndex..13': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.13.1',
-    'PortIndex..14': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.14.1',
-    'PortIndex..15': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.15.1',
-    'PortIndex..16': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.16.1',
-    'PortIndex..17': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.17.1',
-    'PortIndex..18': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.18.1',
-    'PortIndex..19': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.19.1',
-    'PortIndex..20': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.20.1',
-    'PortIndex..21c': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.21.1',
-    'PortIndex..21f': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.21.2',
-    'PortIndex..22c': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.22.1',
-    'PortIndex..22f': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.22.2',
-    'PortIndex..23c': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.23.1',
-    'PortIndex..23f': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.23.2',
-    'PortIndex..24c': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.24.1',
-    'PortIndex..24f': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.24.2',
+    # PortIndex        .1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1		swL2PortInfoPortIndex
+    'PortIndex..1':   '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.1.1',
+    'PortIndex..2':   '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.2.1',
+    'PortIndex..3':   '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.3.1',
+    'PortIndex..4':   '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.4.1',
+    'PortIndex..5':   '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.5.1',
+    'PortIndex..6':   '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.6.1',
+    'PortIndex..7':   '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.7.1',
+    'PortIndex..8':   '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.8.1',
+    'PortIndex..9c':  '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.9.1',
+    'PortIndex..9f':  '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.9.2',
+    'PortIndex..10c': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.10.1',
+    'PortIndex..10f': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.1.10.2',
 }
 
 get_ifName = {
-    #    PortName            .1.3.6.1.2.1.31.1.1.1.1			ifName
-    'PortName.1': '.1.3.6.1.2.1.31.1.1.1.1.1',
-    'PortName.2': '.1.3.6.1.2.1.31.1.1.1.1.2',
-    'PortName.3': '.1.3.6.1.2.1.31.1.1.1.1.3',
-    'PortName.4': '.1.3.6.1.2.1.31.1.1.1.1.4',
-    'PortName.5': '.1.3.6.1.2.1.31.1.1.1.1.5',
-    'PortName.6': '.1.3.6.1.2.1.31.1.1.1.1.6',
-    'PortName.7': '.1.3.6.1.2.1.31.1.1.1.1.7',
-    'PortName.8': '.1.3.6.1.2.1.31.1.1.1.1.8',
-    'PortName.9': '.1.3.6.1.2.1.31.1.1.1.1.9',
+    # PortName      .1.3.6.1.2.1.31.1.1.1.1			ifName
+    'PortName.1':  '.1.3.6.1.2.1.31.1.1.1.1.1',
+    'PortName.2':  '.1.3.6.1.2.1.31.1.1.1.1.2',
+    'PortName.3':  '.1.3.6.1.2.1.31.1.1.1.1.3',
+    'PortName.4':  '.1.3.6.1.2.1.31.1.1.1.1.4',
+    'PortName.5':  '.1.3.6.1.2.1.31.1.1.1.1.5',
+    'PortName.6':  '.1.3.6.1.2.1.31.1.1.1.1.6',
+    'PortName.7':  '.1.3.6.1.2.1.31.1.1.1.1.7',
+    'PortName.8':  '.1.3.6.1.2.1.31.1.1.1.1.8',
+    'PortName.9':  '.1.3.6.1.2.1.31.1.1.1.1.9',
     'PortName.10': '.1.3.6.1.2.1.31.1.1.1.1.10',
-    'PortName.11': '.1.3.6.1.2.1.31.1.1.1.1.11',
-    'PortName.12': '.1.3.6.1.2.1.31.1.1.1.1.12',
-    'PortName.13': '.1.3.6.1.2.1.31.1.1.1.1.13',
-    'PortName.14': '.1.3.6.1.2.1.31.1.1.1.1.14',
-    'PortName.15': '.1.3.6.1.2.1.31.1.1.1.1.15',
-    'PortName.16': '.1.3.6.1.2.1.31.1.1.1.1.16',
-    'PortName.17': '.1.3.6.1.2.1.31.1.1.1.1.17',
-    'PortName.18': '.1.3.6.1.2.1.31.1.1.1.1.18',
-    'PortName.19': '.1.3.6.1.2.1.31.1.1.1.1.19',
-    'PortName.20': '.1.3.6.1.2.1.31.1.1.1.1.20',
-    'PortName.21': '.1.3.6.1.2.1.31.1.1.1.1.21',
-    'PortName.22': '.1.3.6.1.2.1.31.1.1.1.1.22',
-    'PortName.23': '.1.3.6.1.2.1.31.1.1.1.1.23',
-    'PortName.24': '.1.3.6.1.2.1.31.1.1.1.1.24',
 }
 
 get_SinglePort = {
-    #    MediumType          .1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.2		swL2PortInfoMediumType
-    'MediumType..c': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.2.%s.1',
-    'MediumType..f': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.2.%s.2',
-    #    ActualStatus        .1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.5		swL2PortInfoLinkStatus
+    # MediumType        .1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.2		swL2PortInfoMediumType
+    'MediumType..c':   '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.2.%s.1',
+    'MediumType..f':   '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.2.%s.2',
+    # ActualStatus      .1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.5		swL2PortInfoLinkStatus
     'ActualStatus..c': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.5.%s.1',
     'ActualStatus..f': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.5.%s.2',
-    #    ActualSpeed         .1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.6		swL2PortInfoNwayStatus
-    'ActualSpeed..c': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.6.%s.1',
-    'ActualSpeed..f': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.6.%s.2',
-    #    AdminStatus         .1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.4		swL2PortCtrlAdminState
-    'AdminStatus..c': '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.4.%s.1',
-    'AdminStatus..f': '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.4.%s.2',
-    #    AdminSpeed          .1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.5		swL2PortCtrlNwayState
-    'AdminSpeed..c': '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.5.%s.1',
-    'AdminSpeed..f': '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.5.%s.2',
-    #    AdminFlow           .1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.6		swL2PortCtrlFlowCtrlState
-    'AdminFlow..c': '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.6.%s.1',
-    'AdminFlow..f': '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.6.%s.2',
-    #    PortType            .1.3.6.1.2.1.2.2.1.3				ifType
-    'PortType.': '.1.3.6.1.2.1.2.2.1.3.%s',
-    #    PortName            .1.3.6.1.2.1.31.1.1.1.1			ifName
-    'PortName.': '.1.3.6.1.2.1.31.1.1.1.1.%s',
-    #    PortDescr           .1.3.6.1.2.1.31.1.1.1.18			ifAlias
-    #    'PortDescr.'      : '.1.3.6.1.2.1.31.1.1.1.18.%s',
+    # ActualSpeed       .1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.6		swL2PortInfoNwayStatus
+    'ActualSpeed..c':  '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.6.%s.1',
+    'ActualSpeed..f':  '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.6.%s.2',
+    # AdminStatus       .1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.4		swL2PortCtrlAdminState
+    'AdminStatus..c':  '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.4.%s.1',
+    'AdminStatus..f':  '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.4.%s.2',
+    # AdminSpeed        .1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.5		swL2PortCtrlNwayState
+    'AdminSpeed..c':   '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.5.%s.1',
+    'AdminSpeed..f':   '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.5.%s.2',
+    # AdminFlow         .1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.6		swL2PortCtrlFlowCtrlState
+    'AdminFlow..c':    '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.6.%s.1',
+    'AdminFlow..f':    '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.6.%s.2',
+    # PortType          .1.3.6.1.2.1.2.2.1.3				ifType
+    'PortType.':       '.1.3.6.1.2.1.2.2.1.3.%s',
+    # PortName          .1.3.6.1.2.1.31.1.1.1.1			ifName
+    'PortName.':       '.1.3.6.1.2.1.31.1.1.1.1.%s',
+    # PortDescr         .1.3.6.1.2.1.31.1.1.1.18			ifAlias
+    # 'PortDescr.'   : '.1.3.6.1.2.1.31.1.1.1.18.%s',
 }
 
 walk_AllPorts = {
-    #    MediumType          .1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.2		swL2PortInfoMediumType
-    'MediumType': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.2',
-    #    ActualStatus        .1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.5		swL2PortInfoLinkStatus
+    # MediumType     .1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.2		swL2PortInfoMediumType
+    'MediumType':   '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.2',
+    # ActualStatus   .1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.5		swL2PortInfoLinkStatus
     'ActualStatus': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.5',
-    #    ActualSpeed         .1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.6		swL2PortInfoNwayStatus
-    'ActualSpeed': '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.6',
-    #    AdminStatus         .1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.4		swL2PortCtrlAdminState
-    'AdminStatus': '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.4',
-    #    AdminSpeed          .1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.5		swL2PortCtrlNwayState
-    'AdminSpeed': '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.5',
-    #    AdminFlow           .1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.6		swL2PortCtrlFlowCtrlState
-    'AdminFlow': '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.6',
+    # ActualSpeed    .1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.6		swL2PortInfoNwayStatus
+    'ActualSpeed':  '.1.3.6.1.4.1.171.11.133.1.1.2.3.1.1.6',
+    # AdminStatus    .1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.4		swL2PortCtrlAdminState
+    'AdminStatus':  '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.4',
+    # AdminSpeed     .1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.5		swL2PortCtrlNwayState
+    'AdminSpeed':   '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.5',
+    # AdminFlow      .1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.6		swL2PortCtrlFlowCtrlState
+    'AdminFlow':    '.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.6',
 }
 
 walk_ifAlias = {
-    #    PortDescr           .1.3.6.1.2.1.31.1.1.1.18			ifAlias
+    # PortDescr   .1.3.6.1.2.1.31.1.1.1.18			ifAlias
     'PortDescr': '.1.3.6.1.2.1.31.1.1.1.18',
 }
 
 walk_FDB_VLAN = {
-    #    FDB                 .1.3.6.1.2.1.17.7.1.2.2.1.2			dot1qTpFdbPort
+    # FDB   .1.3.6.1.2.1.17.7.1.2.2.1.2			dot1qTpFdbPort
     'FDB': '.1.3.6.1.2.1.17.7.1.2.2.1.2.%s',
 }
 
 walk_VlanMap = {
-    #    VlanName            .1.3.6.1.2.1.17.7.1.4.3.1.1			dot1qVlanStaticName
-    'VlanName': '.1.3.6.1.2.1.17.7.1.4.3.1.1',
-    #    EgressPorts         .1.3.6.1.2.1.17.7.1.4.3.1.2			dot1qVlanStaticEgressPorts
+    # VlanName                 .1.3.6.1.2.1.17.7.1.4.3.1.1			dot1qVlanStaticName
+    'VlanName':               '.1.3.6.1.2.1.17.7.1.4.3.1.1',
+    # EgressPorts              .1.3.6.1.2.1.17.7.1.4.3.1.2			dot1qVlanStaticEgressPorts
     'hex_string:EgressPorts': '.1.3.6.1.2.1.17.7.1.4.3.1.2',
 }
 
 walk_VlanEgressPorts = {
-    #    VEP                 .1.3.6.1.2.1.17.7.1.4.3.1.2			dot1qVlanStaticEgressPorts
+    # VEP              .1.3.6.1.2.1.17.7.1.4.3.1.2			dot1qVlanStaticEgressPorts
     'hex_string:VEP': '.1.3.6.1.2.1.17.7.1.4.3.1.2',
 }
 
 walk_VlanUntaggedPorts = {
-    #    VUP                 .1.3.6.1.2.1.17.7.1.4.3.1.4			dot1qVlanStaticUntaggedPorts
+    # VUP              .1.3.6.1.2.1.17.7.1.4.3.1.4			dot1qVlanStaticUntaggedPorts
     'hex_string:VUP': '.1.3.6.1.2.1.17.7.1.4.3.1.4',
 }
 
 get_CableDiag = {
-    #    cdLinkStatus        .1.3.6.1.4.1.171.12.58.1.1.1.3			swEtherCableDiagLinkStatus
-    'cdLinkStatus.': '.1.3.6.1.4.1.171.12.58.1.1.1.3.%s',
-    #    cdPair1Status       .1.3.6.1.4.1.171.12.58.1.1.1.4			swEtherCableDiagPair1Status
+    # cdLinkStatus     .1.3.6.1.4.1.171.12.58.1.1.1.3			swEtherCableDiagLinkStatus
+    'cdLinkStatus.':  '.1.3.6.1.4.1.171.12.58.1.1.1.3.%s',
+    # cdPair1Status    .1.3.6.1.4.1.171.12.58.1.1.1.4			swEtherCableDiagPair1Status
     'cdPair1Status.': '.1.3.6.1.4.1.171.12.58.1.1.1.4.%s',
-    #    cdPair2Status       .1.3.6.1.4.1.171.12.58.1.1.1.5			swEtherCableDiagPair2Status
+    # cdPair2Status    .1.3.6.1.4.1.171.12.58.1.1.1.5			swEtherCableDiagPair2Status
     'cdPair2Status.': '.1.3.6.1.4.1.171.12.58.1.1.1.5.%s',
-    #    cdPair3Status       .1.3.6.1.4.1.171.12.58.1.1.1.6			swEtherCableDiagPair3Status
+    # cdPair3Status    .1.3.6.1.4.1.171.12.58.1.1.1.6			swEtherCableDiagPair3Status
     'cdPair3Status.': '.1.3.6.1.4.1.171.12.58.1.1.1.6.%s',
-    #    cdPair4Status       .1.3.6.1.4.1.171.12.58.1.1.1.7			swEtherCableDiagPair4Status
+    # cdPair4Status    .1.3.6.1.4.1.171.12.58.1.1.1.7			swEtherCableDiagPair4Status
     'cdPair4Status.': '.1.3.6.1.4.1.171.12.58.1.1.1.7.%s',
-    #    cdPair1Length       .1.3.6.1.4.1.171.12.58.1.1.1.8			swEtherCableDiagPair1Length
+    # cdPair1Length    .1.3.6.1.4.1.171.12.58.1.1.1.8			swEtherCableDiagPair1Length
     'cdPair1Length.': '.1.3.6.1.4.1.171.12.58.1.1.1.8.%s',
-    #    cdPair2Length       .1.3.6.1.4.1.171.12.58.1.1.1.9			swEtherCableDiagPair2Length
+    # cdPair2Length    .1.3.6.1.4.1.171.12.58.1.1.1.9			swEtherCableDiagPair2Length
     'cdPair2Length.': '.1.3.6.1.4.1.171.12.58.1.1.1.9.%s',
-    #    cdPair3Length       .1.3.6.1.4.1.171.12.58.1.1.1.10		swEtherCableDiagPair3Length
+    # cdPair3Length    .1.3.6.1.4.1.171.12.58.1.1.1.10		swEtherCableDiagPair3Length
     'cdPair3Length.': '.1.3.6.1.4.1.171.12.58.1.1.1.10.%s',
-    #    cdPair4Length       .1.3.6.1.4.1.171.12.58.1.1.1.11		swEtherCableDiagPair4Length
+    # cdPair4Length    .1.3.6.1.4.1.171.12.58.1.1.1.11		swEtherCableDiagPair4Length
     'cdPair4Length.': '.1.3.6.1.4.1.171.12.58.1.1.1.11.%s',
 }
 
 get_Errors = {
-    #    CRC                    .1.3.6.1.2.1.16.1.1.1.8			etherStatsCRCAlignErrors
-    'CRC.': '.1.3.6.1.2.1.16.1.1.1.8.%s',
-    #    Undersize              .1.3.6.1.2.1.16.1.1.1.9			etherStatsUndersizePkts
-    'Undersize.': '.1.3.6.1.2.1.16.1.1.1.9.%s',
-    #    Oversize               .1.3.6.1.2.1.16.1.1.1.10			etherStatsOversizePkts
-    'Oversize.': '.1.3.6.1.2.1.16.1.1.1.10.%s',
-    #    Fragment               .1.3.6.1.2.1.16.1.1.1.11			etherStatsFragments
-    'Fragment.': '.1.3.6.1.2.1.16.1.1.1.11.%s',
-    #    Jabber                 .1.3.6.1.2.1.16.1.1.1.12			etherStatsJabbers
-    'Jabber.': '.1.3.6.1.2.1.16.1.1.1.12.%s',
-    #    ExcessiveDefferal      .1.3.6.1.2.1.10.7.2.1.7			dot3StatsDeferredTransmissions
-    'ExcessiveDefferal.': '.1.3.6.1.2.1.10.7.2.1.7.%s',
-    #    LateCollision          .1.3.6.1.2.1.10.7.2.1.8			dot3StatsLateCollisions
-    'LateCollision.': '.1.3.6.1.2.1.10.7.2.1.8.%s',
-    #    ExcessiveCollision     .1.3.6.1.2.1.10.7.2.1.9			dot3StatsExcessiveCollisions
+    # CRC                   .1.3.6.1.2.1.16.1.1.1.8			etherStatsCRCAlignErrors
+    'CRC.':                '.1.3.6.1.2.1.16.1.1.1.8.%s',
+    # Undersize             .1.3.6.1.2.1.16.1.1.1.9			etherStatsUndersizePkts
+    'Undersize.':          '.1.3.6.1.2.1.16.1.1.1.9.%s',
+    # Oversize              .1.3.6.1.2.1.16.1.1.1.10			etherStatsOversizePkts
+    'Oversize.':           '.1.3.6.1.2.1.16.1.1.1.10.%s',
+    # Fragment              .1.3.6.1.2.1.16.1.1.1.11			etherStatsFragments
+    'Fragment.':           '.1.3.6.1.2.1.16.1.1.1.11.%s',
+    # Jabber                .1.3.6.1.2.1.16.1.1.1.12			etherStatsJabbers
+    'Jabber.':             '.1.3.6.1.2.1.16.1.1.1.12.%s',
+    # ExcessiveDefferal     .1.3.6.1.2.1.10.7.2.1.7			dot3StatsDeferredTransmissions
+    'ExcessiveDefferal.':  '.1.3.6.1.2.1.10.7.2.1.7.%s',
+    # LateCollision         .1.3.6.1.2.1.10.7.2.1.8			dot3StatsLateCollisions
+    'LateCollision.':      '.1.3.6.1.2.1.10.7.2.1.8.%s',
+    # ExcessiveCollision    .1.3.6.1.2.1.10.7.2.1.9			dot3StatsExcessiveCollisions
     'ExcessiveCollision.': '.1.3.6.1.2.1.10.7.2.1.9.%s',
-    #    SingleCollision        .1.3.6.1.2.1.10.7.2.1.4			dot3StatsSingleCollisionFrames
-    'SingleCollision.': '.1.3.6.1.2.1.10.7.2.1.4.%s',
-    #    Collision              .1.3.6.1.2.1.16.1.1.1.13			etherStatsCollisions
-    'Collision.': '.1.3.6.1.2.1.16.1.1.1.13.%s',
+    # SingleCollision       .1.3.6.1.2.1.10.7.2.1.4			dot3StatsSingleCollisionFrames
+    'SingleCollision.':    '.1.3.6.1.2.1.10.7.2.1.4.%s',
+    # Collision             .1.3.6.1.2.1.16.1.1.1.13			etherStatsCollisions
+    'Collision.':          '.1.3.6.1.2.1.16.1.1.1.13.%s',
 }
 
 get_InOutOctets = {
-    #    InOctets            .1.3.6.1.2.1.31.1.1.1.6			ifHCInOctets
-    'InOctets.': '.1.3.6.1.2.1.31.1.1.1.6.%s',
-    #    OutOctets           .1.3.6.1.2.1.31.1.1.1.10			ifHCOutOctets
+    # InOctets     .1.3.6.1.2.1.31.1.1.1.6			ifHCInOctets
+    'InOctets.':  '.1.3.6.1.2.1.31.1.1.1.6.%s',
+    # OutOctets    .1.3.6.1.2.1.31.1.1.1.10			ifHCOutOctets
     'Outoctets.': '.1.3.6.1.2.1.31.1.1.1.10.%s',
 }
 
@@ -327,46 +297,46 @@ clear_errors = {
 }
 
 set_AdminStatus = [
-    #     .1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.4				swL2PortCtrlAdminState
+    # .1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.4				swL2PortCtrlAdminState
     ['.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.4.%s', '1', '%s', 'INTEGER'],
 ]
 
 set_AdminSpeed = [
-    #     .1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.5				swL2PortCtrlNwayState
+    # .1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.5				swL2PortCtrlNwayState
     ['.1.3.6.1.4.1.171.11.133.1.1.2.3.2.1.5.%s', '1', '%s', 'INTEGER'],
 ]
 
 set_CableDiagInit = [
-    #     .1.3.6.1.4.1.171.12.58.1.1.1.12					swEtherCableDiagAction
+    # .1.3.6.1.4.1.171.12.58.1.1.1.12					swEtherCableDiagAction
     ['.1.3.6.1.4.1.171.12.58.1.1.1.12', '%s', '1', 'INTEGER'],
 ]
 
 set_SaveConfig = [
-    #     .1.3.6.1.4.1.171.12.1.2.18.4					agentBscFileSystemSaveCfg
+    # .1.3.6.1.4.1.171.12.1.2.18.4					agentBscFileSystemSaveCfg
     ['.1.3.6.1.4.1.171.12.1.2.18.4', '0', '2', 'INTEGER'],
 ]
 
 set_CreateVlan = [
-    #     .1.3.6.1.2.1.17.7.1.4.3.1.1					dot1qVlanStaticName
+    # .1.3.6.1.2.1.17.7.1.4.3.1.1					dot1qVlanStaticName
     ['.1.3.6.1.2.1.17.7.1.4.3.1.1', '{1}', '{2}', 'OCTETSTR'],
-    #     .1.3.6.1.2.1.17.7.1.4.3.1.2					dot1qVlanStaticEgressPorts
+    # .1.3.6.1.2.1.17.7.1.4.3.1.2					dot1qVlanStaticEgressPorts
     ['.1.3.6.1.2.1.17.7.1.4.3.1.2', '{1}', '{3}', 'OCTETSTR'],
-    #     .1.3.6.1.2.1.17.7.1.4.3.1.5					dot1qVlanStaticRowStatus
+    # .1.3.6.1.2.1.17.7.1.4.3.1.5					dot1qVlanStaticRowStatus
     ['.1.3.6.1.2.1.17.7.1.4.3.1.5', '{1}', '4', 'INTEGER'],
 ]
 
 set_IpifCfg = [
-    #     .1.3.6.1.2.1.16.19.11.1.1						netConfigIPAddress
+    # .1.3.6.1.2.1.16.19.11.1.1						netConfigIPAddress
     ['.1.3.6.1.2.1.16.19.11.1.1', '5121', '{1}', 'IPADDR'],
-    #     .1.3.6.1.2.1.16.19.11.1.2						netConfigSubnetMask
+    # .1.3.6.1.2.1.16.19.11.1.2						netConfigSubnetMask
     ['.1.3.6.1.2.1.16.19.11.1.2', '5121', '{2}', 'IPADDR'],
-    #     .1.3.6.1.2.1.16.19.12						netDefaultGateway
+    # .1.3.6.1.2.1.16.19.12						netDefaultGateway
     ['.1.3.6.1.2.1.16.19.12', '0', '{3}', 'IPADDR'],
-    #     .1.3.6.1.4.1.171.11.133.1.1.2.1.2.16				swL2DevCtrlManagementVlanId
+    # .1.3.6.1.4.1.171.11.133.1.1.2.1.2.16				swL2DevCtrlManagementVlanId
     ['.1.3.6.1.4.1.171.11.133.1.1.2.1.2.16', '0', '{4}', 'INTEGER'],
 ]
 
 set_DHCP_RemoteID = [
-    #     .1.3.6.1.4.1.171.12.42.3.2.5					swDHCPRelayOption82RemoteID
+    # .1.3.6.1.4.1.171.12.42.3.2.5					swDHCPRelayOption82RemoteID
     ['.1.3.6.1.4.1.171.12.42.3.2.5', '0', '{1}', 'OCTETSTR'],
 ]
